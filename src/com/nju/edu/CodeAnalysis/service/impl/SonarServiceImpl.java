5d79311f0a9e31b18690e96c6911a9ebd39514b5
zhaohongru <635351883@qq.com>
2019-05-07 18:30:53 +0800

    hashmap的接口

--- src/com/nju/edu/CodeAnalysis/service/impl/SonarServiceImpl.java
+++ src/com/nju/edu/CodeAnalysis/service/impl/SonarServiceImpl.java
@@ -3,6 +3,7 @@ package com.nju.edu.CodeAnalysis.service.impl;
 import com.nju.edu.CodeAnalysis.service.SonarService;
 
 import java.net.URL;
+import java.util.Map;
 import java.util.HashMap;
 
 public class SonarServiceImpl implements SonarService {
@@ -16,7 +17,15 @@ public class SonarServiceImpl implements SonarService {
 	
 	@Override
 	public HashMap<String, String> getAnalysis(String studentID, String courseName, int number){
-		
+		HashMap<String, String> map = new HashMap<String, String>();
+		map.put("coverage", "0.0");
+		map.put("duplicated_lines_density", "0.0");
+		map.put("coverage", "0.0");
+		map.put("vulnerabilities", "0.0");
+		map.put("ncloc_language_distribution", "java=232;xml=122");
+		map.put("bugs", "0");
+		map.put("code_smells", "49");
+		return map;
 	}
 
 }
